e299c958ba71adca1b5253cf4471fe577292380a
Eliran Haziza <eliranhaze@gmail.com>
2021-07-03 18:15:07 -0400

    new and improved test positions

--- speedtest.py
+++ speedtest.py
@@ -4,25 +4,30 @@ import sys
 import time
 
 engine = importlib.import_module(sys.argv[1])
-
+depth = int(sys.argv[2])
 
 e = engine.Engine()
 e.LOG = 0
 e.PRINT = 0
 e.DISPLAY = 0
 e.ITERATIVE = 1
-e.MAX_ITER_DEPTH = 5
+e.MAX_ITER_DEPTH = depth
+e.DEPTH = depth
 e.move_time_limit = 2000
 e.book = 0
 
-def test(fen):
+def test(fen, play = 0):
     e.set_fen(fen)
     t0 = time.time()
+    for _ in range(play):
+        e._play_move()
     move = e._select_move()
     print('selected %s [%.3fs]' % (e.board.san(move), time.time() - t0))
     print('nodes visited:', e.nodes)
     print('positions evaluated:', e.ev - e.tt)
     print('evaluations fetched:', e.tt)
+
+# Might want to add some more from here: https://www.chessprogramming.org/Test-Positions
 
 # start position
 test(chess.Board().fen())
@@ -36,8 +41,20 @@ test('rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2')
 # a position in the sicilian, as white
 test('rnbqkb1r/1p3ppp/p2p1n2/4p3/3NP3/2N1B3/PPP2PPP/R2QKB1R w KQkq - 0 7')
 
+# a queen's gambit position
+test('rn1qk2r/p3bppp/bpp1pn2/3p4/2PP4/1PB2NP1/P3PPBP/RN1QK2R w KQkq - 0 9')
+
 # an endgame position
 test('8/p6p/1p1Pk3/5p1p/1P3K1P/6P1/5P2/8 b - - 0 46')
 
 # mate in 2
-test('1r3b1k/5Qp1/p6B/q1Pp4/2p5/5P1P/P1K2P2/3R2R1 b - - 2 27')
+test('1r3b1k/5Qp1/p6B/q1Pp4/2p5/5P1P/P1K2P2/3R2R1 b - - 2 27', play = 2)
+
+# a sicilian position, play 3 moves
+test('r1bqk2r/pp1pppbp/2n2np1/1Bp5/4P3/5N2/PPPP1PPP/RNBQR1K1 w kq - 4 6', play = 3)
+
+# queen's gambit position, play 3 moves
+test('r1bqkb1r/pp1n1ppp/2p1pn2/3p2B1/2PP4/2N1PN2/PP3PPP/R2QKB1R b KQkq - 1 6', play = 3)
+
+# easy mate in 3, play to mate (depth >= 5 needed to find mate)
+test('r5rk/5p1p/5R2/4B3/8/8/7P/7K w', play = 4)
