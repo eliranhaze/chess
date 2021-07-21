e9a87d0c2fd2e0a9719af5458eac5bcd417309a8
Eliran Haziza <eliranhaze@gmail.com>
2021-07-21 11:46:27 -0400

    new king function in board

--- board.py
+++ board.py
@@ -1,6 +1,12 @@
 import chess
 
+msb = chess.msb
+
 class Board(chess.Board):
 
     def __init__(self, *arg, **kw):
         super().__init__(*arg, **kw)
+
+    def king(self, color):
+        """ returns king square """
+        return msb(self.occupied_co[color] & self.kings)
